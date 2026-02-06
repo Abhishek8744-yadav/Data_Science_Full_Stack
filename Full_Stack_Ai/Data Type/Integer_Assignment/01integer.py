9c98f524230e9f1bf69077050c4b6e1c38ae637f
Abhishek8744-yadav <yadav.200426@gmail.com>
2026-02-06 15:40:01 +0530

    Add files via upload

---
+++ Full_Stack_Ai/Data Type/Integer_Assignment/01integer.py
@@ -0,0 +1,9 @@
+# Question 1: Calculate the product of first 10 natural numbers
+print("Question 1: Calculate the product of first 10 natural numbers")
+
+product=1
+
+for i in range(1,11):
+    product=product*i
+
+print(f"The Product of 10 natural number is :- {product}")
