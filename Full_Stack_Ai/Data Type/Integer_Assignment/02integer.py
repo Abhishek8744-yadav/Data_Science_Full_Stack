e2485514d121a59cbe28189bc3a2710756bd9d8a
Abhishek8744-yadav <yadav.200426@gmail.com>
2026-02-09 10:00:39 +0530

    Add files via upload

---
+++ Full_Stack_Ai/Data Type/Integer_Assignment/02integer.py
@@ -0,0 +1,10 @@
+# Question 2: Find the remainder when 156 is divided by 7
+print("\nQuestion 2: Find the remainder when 156 is divided by 7")
+
+num1 = 156
+
+num2 = 7
+
+reminder = 156%7
+
+print(f"The Reminder is {reminder}")
