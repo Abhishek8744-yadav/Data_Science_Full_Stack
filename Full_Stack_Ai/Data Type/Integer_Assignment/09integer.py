bbdfc03535688179afa6d8325ce021548d1133a9
Abhishek8744-yadav <yadav.200426@gmail.com>
2026-02-09 10:04:05 +0530

    Add files via upload

---
+++ Full_Stack_Ai/Data Type/Integer_Assignment/09integer.py
@@ -0,0 +1,7 @@
+# Question 9: Find the greatest common divisor (GCD) of 48 and 36
+print("\nQuestion 9: Find the greatest common divisor (GCD) of 48 and 36")
+
+import math
+
+Answer= math.gcd(48,36)
+print(Answer)
