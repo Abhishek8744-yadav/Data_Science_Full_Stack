eecfe77aac1a244b6b2285903c697c451870b1c4
Abhishek8744-yadav <yadav.200426@gmail.com>
2026-02-20 21:53:46 +0530

    Add files via upload

---
+++ Full_Stack_Ai/Data Type/Set_Assigment/01set.py
@@ -0,0 +1,9 @@
+# Question 1: Create a set of first 10 natural numbers
+print("Question 1: Create a set of first 10 natural numbers")
+
+first_natural=set()
+
+for num in range(1,11):
+    first_natural.add(num)
+
+print(f"First 10 Natural number is:- {first_natural}")
