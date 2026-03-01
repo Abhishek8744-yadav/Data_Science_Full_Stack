57869032991cdcce973cc64cc8e8b2f39c3ec65d
Abhishek Yadav <yadav.200426@gmail.com>
2026-03-01 16:02:31 +0530

    Add files via upload

---
+++ Sql_Assignment/01_String.sql
@@ -0,0 +1,51 @@
+CREATE TABLE Orders (
+    Order_ID INT PRIMARY KEY,
+    Order_Date DATE,
+    Customer_ID VARCHAR(10),
+    Customer_Name VARCHAR(100),
+    Product_Category VARCHAR(100),
+    Product_Name VARCHAR(150),
+    Quantity INT,
+    Unit_Price DECIMAL(10,2),
+    Payment_Mode VARCHAR(50),
+    Store_Location VARCHAR(100)
+);
+
+
+INSERT ALL
+    INTO Orders VALUES (1001, TO_DATE('2026-02-01','YYYY-MM-DD'), 'C001', 'Ravi Kumar', 'Electronics', 'Wireless Mouse', 2, 799, 'UPI', 'Bangalore')
+    INTO Orders VALUES (1002, TO_DATE('2026-02-02','YYYY-MM-DD'), 'C002', 'Sneha Reddy', 'Grocery', 'Basmati Rice 5kg', 1, 650, 'Credit Card', 'Hyderabad')
+    INTO Orders VALUES (1003, TO_DATE('2026-02-03','YYYY-MM-DD'), 'C003', 'Arjun Mehta', 'Fashion', 'Men''s T-Shirt', 3, 499, 'Cash', 'Chennai')
+    INTO Orders VALUES (1004, TO_DATE('2026-02-04','YYYY-MM-DD'), 'C004', 'Priya Sharma', 'Electronics', 'Bluetooth Speaker', 1, 1499, 'Debit Card', 'Mumbai')
+    INTO Orders VALUES (1005, TO_DATE('2026-02-05','YYYY-MM-DD'), 'C005', 'Kiran Rao', 'Home Kitchen', 'Mixer Grinder', 1, 2499, 'UPI', 'Bangalore')
+    INTO Orders VALUES (1006, TO_DATE('2026-02-06','YYYY-MM-DD'), 'C006', 'Neha Verma', 'Beauty', 'Face Cream', 4, 299, 'Credit Card', 'Delhi')
+    INTO Orders VALUES (1007, TO_DATE('2026-02-07','YYYY-MM-DD'), 'C007', 'Rahul Das', 'Grocery', 'Cooking Oil 1L', 5, 180, 'Cash', 'Kolkata')
+    INTO Orders VALUES (1008, TO_DATE('2026-02-08','YYYY-MM-DD'), 'C008', 'Anjali Nair', 'Fashion', 'Women''s Jeans', 2, 1199, 'UPI', 'Kochi')
+    INTO Orders VALUES (1009, TO_DATE('2026-02-09','YYYY-MM-DD'), 'C009', 'Suresh Patel', 'Electronics', 'Smartphone', 1, 15999, 'Debit Card', 'Ahmedabad')
+    INTO Orders VALUES (1010, TO_DATE('2026-02-10','YYYY-MM-DD'), 'C010', 'Meena Iyer', 'Home  Kitchen', 'Pressure Cooker', 1, 1899, 'Credit Card', 'Pune')
+SELECT * FROM DUAL;
+
+COMMIT;
+
+-- # NUMERICAL FUNCTIONS – 20 Questions
+
+-- 1. Round Unit_Price to 2 decimal places.
+-- 2. Find total sales per order.
+-- 3. Calculate average order value.
+-- 4. Find highest product price.
+-- 5. Find lowest product price.
+-- 6. Calculate percentage discount applied.
+-- 7. Find modulus of Quantity divided by 2.
+-- 8. Convert negative values to positive.
+-- 9. Truncate price without rounding.
+-- 10. Find square root of total sales.
+-- 11. Calculate exponential value of a number.
+-- 12. Calculate power of 2^5.
+-- 13. Find absolute difference between two prices.
+-- 14. Calculate sales growth percentage.
+-- 15. Find random number between 1 and 100.
+-- 16. Divide total sales by number of orders.
+-- 17. Find ceiling value of price.
+-- 18. Find floor value of price.
+-- 19. Convert decimal to integer.
+-- 20. Calculate compound interest.
